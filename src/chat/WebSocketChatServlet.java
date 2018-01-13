72eedca4888945213319894a7acf338ab12a56db
Vladimir Ferulev <ferylev@list.ru>
2018-01-13 19:46:08 +0300

    Initial commit

---
+++ src/chat/WebSocketChatServlet.java
@@ -0,0 +1,21 @@
+package chat;
+
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "WebSocketChatServlet", urlPatterns = {"/chat"})
+public class WebSocketChatServlet extends WebSocketServlet {
+    public final static String CHAT_URL = "/chat";
+    private final ChatService chatService;
+
+    public WebSocketChatServlet() {
+        this.chatService = new ChatService();
+    }
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.setCreator((req, resp) -> new ChatWebSocket(chatService));
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        final int port = 8080;
+
+        Server server = new Server(port);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), WebSocketChatServlet.CHAT_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+}
